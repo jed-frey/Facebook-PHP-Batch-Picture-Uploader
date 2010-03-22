9f4ce736b824141617d68f9bca54750f20f85ce2
Jedediah Frey <jafrey@eskimos.dreamhost.com>
2010-03-22 10:53:55 -0700

    Test

--- php_batch_uploader.php
+++ php_batch_uploader.php
@@ -1,6 +1,7 @@
 #!/opt/local/bin/php
 <?php
 # Batch Branch
+# Test
 
 $converterPath = NULL; # To permanently change the image converter, set it here, otherwise use -c on the command line to set it.
 ####
