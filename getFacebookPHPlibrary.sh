62c2594d1f3d2f5ea08d20f4a65cc5a6564a350f
Jedediah Frey <github@exstatic.org>
2010-11-23 20:02:15 -0500

    Fixed line ending in getFacebookPHPLibrary.sh

--- getFacebookPHPlibrary.sh
+++ getFacebookPHPlibrary.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -O "http://svn.facebook.com/svnroot/platform/clients/packages/facebook-platform.tar.gz"
+curl -OL "http://github.com/facebook/platform/raw/master/clients/packages/facebook-platform.tar.gz"
 tar -xvf facebook-platform.tar.gz
 rm facebook-platform.tar.gz
