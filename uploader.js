0a36d28dacebcce08511892ed9fa675342d86036
Jedediah Frey <github@exstatic.org>
2011-03-31 11:08:05 -0700

    Working on sucess pages

---
+++ uploader.js
@@ -0,0 +1,4 @@
+function openAuth(url) {
+	alert(url);
+	window.open(url,'authWindw','width=400,height=200,resizeable=no,scrollbars=no,toolbar=no,location=no,status=no,directories=no,menubar=no,copyhistory=no')
+}
