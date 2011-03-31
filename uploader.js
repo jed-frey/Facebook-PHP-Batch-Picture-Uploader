2a4679dfa34dad3e790401999f22f8d2293f383f
Jedediah Frey <github@exstatic.org>
2011-03-31 11:10:37 -0700

    Fixed height/width.

--- uploader.js
+++ uploader.js
@@ -1,4 +1,4 @@
 function openAuth(url) {
 	alert(url);
-	window.open(url,'authWindw','width=400,height=200,resizeable=no,scrollbars=no,toolbar=no,location=no,status=no,directories=no,menubar=no,copyhistory=no')
+	window.open(url,'authWindw','width=1000,height=400,resizeable=no,scrollbars=no,toolbar=no,location=no,status=no,directories=no,menubar=no,copyhistory=no')
 }
