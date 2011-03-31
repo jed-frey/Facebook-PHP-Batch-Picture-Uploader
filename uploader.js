0bf93bd20640ac330795cebb7df33e0a6b111d64
Jedediah Frey <github@exstatic.org>
2011-03-31 12:21:31 -0700

    Added optional steps of direct upload

--- uploader.js
+++ uploader.js
@@ -1,4 +1,3 @@
 function openAuth(url) {
-	alert(url);
 	window.open(url,'authWindw','width=1000,height=400,resizeable=no,scrollbars=no,toolbar=no,location=no,status=no,directories=no,menubar=no,copyhistory=no')
 }
